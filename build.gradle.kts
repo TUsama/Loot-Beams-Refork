3201df588eec8bbc4f675b402999aee94e26f745
TUsama <1716976420@qq.com>
2025-06-06 17:17:52 +0800

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ fun prop(name: String, consumer: (prop: String) -> Unit) {
 }
 
 
-val modv = "3.2.0"
+val modv = "3.2.1"
 
 
 val loader = when {
--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+### V3.2.1
+fix reforged compat
+
 ### V3.2.0
 change the version for the consistency
 
